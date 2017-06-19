b2653788b1585c6d9a78c375afda93c3cb033d1c
Emmanuel <emmanuel.haastrup@gmail.com>
2017-06-19 14:32:00 +0000

    emmanuel has done the employee, manager and person class

---
+++ src/main/scala/Employee.scala
@@ -0,0 +1,10 @@
+import java.util.Date
+
+/**
+  * Created by Administrator on 19/06/2017.
+  */
+case class Employee (ID: Int, fName: String, lName: String, Age: Int, Gender: String,
+                     salary: Int, loginName: String, password: String) extends Person {
+
+
+}
