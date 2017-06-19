54c0632109335fcbe9f9ec0a6596c686b0e4a187
ericohwotu <eric.ohwotu@outlook.com>
2017-06-19 16:05:38 +0000

    Initial Main Object

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -18,29 +18,57 @@ object Main {
   val loggedIn: Employee = null
 
   //entry point
-  def main(args: Array[String]): Unit = null
+  def main(args: Array[String]): Unit = {}
+
 
   def login(username: String, Password: String): Boolean = false
 
-  def createEmployee(id: Int, name: String, employeeType: EmployeeType.Value): Boolean = false
+  /*
+  * Employee Managemment
+  *
+  * */
+  def createEmployee(ID: Int, fName: String, lName: String, Age: Int, Gender: String, salary: Int, loginName: String, password: String, employeeType: EmployeeType.Value): Boolean = false
+
   def getEmployee(id: Int): Employee = null
+
   def deleteEmployee(id: Int): Boolean = false
 
-  def createTransaction(id: Int, stocks: List[Stock], employee: Employee, time: Date): Boolean = false
+  def getManager(id: Int): Manager = null
+
+  def deleteManager(id: Int): Boolean = false
+
+  /*
+  * Transaction functions
+  *
+  * */
+  def createTransaction(id: Int, employee: Employee, dateAndTime: Date): Boolean = false
+
   def getTransaction(id: Int): Transaction = null
+
   def deleteTransaction(id: Int): Boolean = false
 
+  /*
+  * Report functions
+  *
+  * */
+  def createReport(id: Int, dateFrom: Date, dateTo: Date) = Boolean
 
-  def createReport(id: Int, transactions: List[Transaction]) = Boolean
   def getReport(id: Int): Report = null
+
   def deleteReport(id: Int): Boolean = false
 
-  // create stock depending on parameters
+  /*
+  * Transaction functions
+  *
+  * */
   def createStock(id: Int, name: String, desc: String, price: Double, config: String): Boolean = false
-  def createStock(id: Int, name: String, desc: String, price: Double, itemType: ItemType.Value): Boolean = false
-  def createStock(id: Int, name: String, desc: String, price: Double, rating: Int, genre: String, console: Console): Boolean = false
-  def getStock(id: Int): Stock = null
-  def deleteStock(id: Int): Boolean = false
 
+  def createStock(id: Int, name: String, desc: String, price: Double, itemType: ItemType.Value): Boolean = false
+
+  def createStock(id: Int, name: String, desc: String, price: Double, rating: Int, genre: String, console: Console): Boolean = false
+
+  def getStock(id: Int): Stock = null
+
+  def deleteStock(id: Int): Boolean = false
 
 }
