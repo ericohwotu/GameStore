de26049c20d2497258f0052d9e4659993ed04e54
TheDeadlyPianist <duane221@yahoo.co.uk>
2017-06-20 11:59:12 +0100

    commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,20 +8,20 @@ import scala.collection.mutable.ListBuffer
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 
-object Main extends JFXApp{
-
-  //variable declaration
-  val transactions: ListBuffer[Transaction] = new ListBuffer[Transaction]
-  val stocks: ListBuffer[Stock] = new ListBuffer[Stock]
-  val employees: ListBuffer[Employee] = new ListBuffer[Employee]
-  val managers: ListBuffer[Manager] = new ListBuffer[Manager]
-  val reports: ListBuffer[Report] = new ListBuffer[Report]
-  var loggedIn: Employee = null
+object Main extends JFXApp {
+	
+	//variable declaration
+	val transactions:ListBuffer[Transaction] = new ListBuffer[Transaction]
+	val stocks:ListBuffer[Stock] = new ListBuffer[Stock]
+	val employees:ListBuffer[Employee] = new ListBuffer[Employee]
+	val managers:ListBuffer[Manager] = new ListBuffer[Manager]
+	val reports:ListBuffer[Report] = new ListBuffer[Report]
+	var loggedIn:Employee = null
 	setWindow("login")
 	
-  def Main(args: Array[String]): Unit = {}
-  
-	def setWindow(window:String):Unit ={
+	def Main(args:Array[String]):Unit = {}
+	
+	def setWindow(window:String):Unit = {
 		window match {
 			case "login" => new UIloginWindow
 			case "main" => new UImainWindow
@@ -32,11 +32,10 @@ object Main extends JFXApp{
 			case "3" =>
 		}
 	}
-
-  def login(username: String, Password: String): Boolean = true
 	
-	def logout:Unit ={
+	def login(username:String, Password:String):Boolean = true
+	
+	def logout:Unit = {
 		loggedIn = null
 	}
-
 }
