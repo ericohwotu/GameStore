80c43144ebe7b5e7a28afbd2363c1e4cdccccfe3
Emmanuel <emmanuel.haastrup@gmail.com>
2017-06-20 14:09:49 +0000

    emmanuel has done the employee, manager and person class

--- src/main/scala/TransactionActions.scala
+++ src/main/scala/TransactionActions.scala
@@ -3,14 +3,23 @@ import java.util.Date
 /**
   * Created by Administrator on 19/06/2017.
   */
-trait TransactionActions {
+trait TransactionActions extends MainVariables {
   /*
   * Transaction functions
   *
   * */
-  def createTransaction(id: Int, employee: Employee, dateAndTime: Date): Boolean = false
+  def createTransaction(id: Int, employee: Employee, dateAndTime: Date): Boolean = {
+if (employee == null) false
+else {
+  new Transaction(id, employee, dateAndTime)
+  true
+  new Transaction(id, employee, dateAndTime) =
+}
+
+  }
 
   def getTransaction(id: Int): Transaction = null
+
 
   def deleteTransaction(id: Int): Boolean = false
 
