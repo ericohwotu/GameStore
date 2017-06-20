7276bb0ec138d53859d629005dbf3c9e2a7b20df
TheDeadlyPianist <duane221@yahoo.co.uk>
2017-06-20 11:49:39 +0100

    commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,22 +6,30 @@ import java.util.Date
 
 import scala.collection.mutable.ListBuffer
 import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
 
 object Main extends JFXApp{
-
-  //variable declaration
-  val transactions: ListBuffer[Transaction] = new ListBuffer[Transaction]
-  val stocks: ListBuffer[Stock] = new ListBuffer[Stock]
-  val employees: ListBuffer[Employee] = new ListBuffer[Employee]
-  val managers: ListBuffer[Manager] = new ListBuffer[Manager]
-  val reports: ListBuffer[Report] = new ListBuffer[Report]
-  val loggedIn: Employee = null
-  var loginWindow:UIloginWindow = new UIloginWindow
-  stage = loginWindow
-  
+	
+	setWindow("login")
+	
   def Main(args: Array[String]): Unit = {}
   
+	def setWindow(window:String):Unit ={
+		window match {
+			case "login" => new UIloginWindow
+			case "main" => new UImainWindow
+			case "transaction" => println("Transaction open")
+			case "report" =>
+			case "1" =>
+			case "2" =>
+			case "3" =>
+		}
+	}
 
-  def login(username: String, Password: String): Boolean = false
+  def login(username: String, Password: String): Boolean = true
+	
+	def logout:Unit ={
+		loggedIn = null
+	}
 
 }
---
+++ src/main/scala/test.scala
@@ -0,0 +1,6 @@
+/**
+	* Created by duane on 20/06/2017.
+	*/
+trait test {
+	
+}
