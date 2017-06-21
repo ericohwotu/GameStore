59cbbd73f98ad6002f1ced3647ffcc05941a1505
Emmanuel <emmanuel.haastrup@gmail.com>
2017-06-21 14:01:48 +0000

    emmanuel has done the employee, manager and person class done the testing for transactions

--- src/test/scala/TransactionActionsTests.scala
+++ src/test/scala/TransactionActionsTests.scala
@@ -11,7 +11,7 @@ class TransactionActionsTests extends FlatSpec with Matchers with TransactionAct
 
   //creating the transaction
   "creating new Transaction" should "return true if successful" in {
-    loggedIn = Manager(1, "Emmanuel", "Haastrup", 28, "Male", 60000, "LordManny", "LordOfSmallLords")
+//    loggedIn = Manager(1, "Emmanuel", "Haastrup", 28, "Male", 60000, "LordManny", "LordOfSmallLords")
     val employee = Employee(1,"Eric","Ohwotu",28,"Male",67000,"CallMeMaster","NoneYaBusiness")
     createTransaction(1,employee,new Date()) should be (true)
   }
