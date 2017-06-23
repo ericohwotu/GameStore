9c235a7fd1596ac50c9779154b7cfa7c4f93a9d3
ericohwotu <eric.ohwotu@outlook.com>
2017-06-23 09:52:47 +0000

    added customers creation and and deletion

--- src/main/scala/UIsplashScreen.scala
+++ src/main/scala/UIsplashScreen.scala
@@ -1,33 +1,48 @@
+import java.util.concurrent
+
 import scala.concurrent.{ExecutionContext, Future}
 import scalafx.Includes._
+import scalafx.application.Platform
 import scalafx.scene.Scene
 import scalafx.scene.image.{Image, ImageView}
 import scalafx.scene.input.{KeyCode, KeyEvent}
 import scalafx.scene.paint.Color
 
 /**
-	* Created by duane on 22/06/2017.
-	*/
-class UIsplashScreen extends Scene{
-	
-	val elliotAndFriends:Image = new Image("file:src/main/images/ElliotAndFriends.png")
-	val elliotAndFriendsHolder:ImageView = new ImageView(elliotAndFriends)
-	
-	fill = Color.Transparent
-	
-	content = List(elliotAndFriendsHolder)
-	
-	onKeyPressed = (e:KeyEvent) => {
-		if(e.code == KeyCode.Enter) {
-			Main.setWindow("login")
-		}
-	}
+  * Created by duane on 22/06/2017.
+  */
+class UIsplashScreen extends Scene {
+  var cancelled = false
 
-	implicit val ec = ExecutionContext.global
+  val elliotAndFriends: Image = new Image("file:src/main/images/ElliotAndFriends.png")
+  val elliotAndFriendsHolder: ImageView = new ImageView(elliotAndFriends)
 
-	Future{
-		Thread.sleep(2000)
-		Main.setWindow("login")
-	}
-	
+  fill = Color.Transparent
+
+  content = List(elliotAndFriendsHolder)
+
+  onKeyPressed = (e: KeyEvent) => {
+    if (e.code == KeyCode.Enter) {
+      cancelled = true
+      Main.setWindow("login")
+    }
+  }
+
+  implicit val ec = ExecutionContext.global
+
+  Future {
+    Thread.sleep(10000)
+    if (Platform.isFxApplicationThread) {
+      Main.setWindow("login")
+    } else {
+      val callable = new concurrent.Callable[Unit]
+      {
+        def call(): Unit = if(!cancelled)Main.setWindow("login")
+      }
+      val future = new concurrent.FutureTask(callable)
+      Platform.runLater(future)
+      future.get()
+    }
+  }
+
 }
